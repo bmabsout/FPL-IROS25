d91d3e1cd557267b8f5424d0e7b35317429c012d
AbdelrahmanAbdelgwad <aaoaa@bu.edu>
2024-12-20 17:53:26 -0500

    modified num of epochs

--- cmorl/train_all.py
+++ cmorl/train_all.py
@@ -71,5 +71,5 @@ import os
 environments = ["Reacher-v4", "Hopper-v4", "Walker2d-v4", "HalfCheetah-v4", "Pendulum-v1", "LunarLanderContinuous-v2"]
 
 for env in environments:
-    cmd = f"python -m cmorl.hyper_search {env} --experiment_name {env}_AQS --steps_per_epoch 2000 --epochs 2 --start_steps 10000 --num_seeds 10"
+    cmd = f"python -m cmorl.hyper_search {env} --experiment_name {env}_AQS --steps_per_epoch 2000 --epochs 250 --start_steps 10000 --num_seeds 10"
     os.system(cmd)
